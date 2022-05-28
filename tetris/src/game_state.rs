5de2fafbedd90f7b2f2d9d65b637facdb0a578d8
Wouter <wouter.kerkhofs@telenet.be>
2022-05-28 13:22:40 +0200

    addind starting positions to well

--- tetris/src/game_state.rs
+++ tetris/src/game_state.rs
@@ -1,13 +1,17 @@
 // keep track of the state of the game during play
-use crate::{well::{Well, NewWell}, block_bag::{BlockBag, RandomBag}, block::Block};
+use crate::{well::{Well, WellDefaults}, block_bag::{BlockBag, RandomBag}, block::Block};
 
 pub struct GameState {
     game_over: bool,
+
     well: Well,
+    
     block_bag: BlockBag,
     current_block: Block,
-    next_block: Block
+    next_block: Block,
 
+    block_row: u8,
+    block_col: u8,
     //todo wouter: fill when relevant
 }
 
@@ -16,13 +20,17 @@ impl GameState {
         let mut block_bag : BlockBag = RandomBag::get();
         let current = block_bag.pop().unwrap();
         let next = block_bag.pop().unwrap();
+        let well = WellDefaults::new();
+        let starting_point = WellDefaults::get_start_position(&well);
 
         GameState { 
             game_over: false, 
-            well: NewWell::new(),
+            well,
             block_bag,
             current_block: current,        
             next_block: next,
+            block_row: starting_point.row_ix,
+            block_col: starting_point.col_ix
         }            
     }
 }
--- tetris/src/well.rs
+++ tetris/src/well.rs
@@ -1,19 +1,54 @@
 // a Tetris-playfield is called a well
 // the playfield is 10 columns wide and 24 rows high
 // so we create a 10x24-matrix
-// (inner array = rows - spread over 24 columns)
+// inner array = rows - spread over 24 columns
+// low row = high up the well
 pub type Well = [[u8; 10]; 24];
+
+const START_ROW : u8 = 2;
+const START_COL : u8 = 3;
+
+pub struct WellPoint {
+    pub row_ix: u8,
+    pub col_ix: u8
+}
+
+pub trait WellDefaults {
+    fn new() -> Self;   
+    fn get_start_position(&self) -> WellPoint;
+}
+
+impl WellDefaults for Well {
+    fn new() -> Well {
+        empty_well()
+    }
+
+    fn get_start_position(&self) -> WellPoint {
+        WellPoint { row_ix: START_ROW, col_ix: START_COL }
+    }
+}
 
 fn empty_well() -> Well {
     [[0u8; 10]; 24]
 }
 
-pub trait NewWell {
-    fn new() -> Self;
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
+        
+    #[test]
+    fn well_new() {
+        let new_well: Well = WellDefaults::new();
 
-impl NewWell for Well {
-    fn new() -> Well {
-        empty_well()
+        assert_eq!(new_well, [[0u8; 10]; 24]);            
     }
+
+    #[test]
+    fn well_starting_positions() {
+        let new_well: Well = WellDefaults::new();
+        let starting_position = WellDefaults::get_start_position(&new_well);
+
+        assert_eq!(starting_position.row_ix, START_ROW);
+        assert_eq!(starting_position.col_ix, START_COL);      
+    }     
 }
